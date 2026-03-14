7ee13547edba5c98eaae4e04ce89d9b55a20fe11
SJB7788 <sbj7788@gmail.com>
2026-03-14 10:58:12 -0700

    add basic funcitons for baseline

--- main.go
+++ main.go
@@ -7,6 +7,40 @@ import (
 	tea "charm.land/bubbletea/v2"
 )
 
+type model struct {
+	value int
+}
+
+func InitialModel() model {
+	return model{
+		value: 0,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	return tea.NewView("Hello World")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	p := tea.NewProgram(InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error occurred: %v", err)
+		os.Exit(1)
+	}
 }
