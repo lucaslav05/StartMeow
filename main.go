06d3383b9262f05373b2c42d9512d9f7e072d7c5
Brandon Rada <brandonn.rada@gmail.com>
2026-03-14 00:57:53 -0700

    Completed an initial basic generation for a webapp template. Will check tomorrow if this is kinda what we want.

--- main.go
+++ main.go
@@ -4,9 +4,41 @@ import (
 	"fmt"
 	"os"
 
-	tea "charm.land/bubbletea/v2"
+// 	tea "charm.land/bubbletea/v2"
+	"StartMeow/generator"
 )
 
+func usage(){
+    fmt.Println("Usage: StartMeow <project-name> [--force]")
+    return
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+// 	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+	    usage()
+	    return
+	}
+
+    projectName := os.Args[1]
+    force := false
+
+    // Check for --force flag
+    if len(os.Args) > 2 && os.Args[2] == "--force" {
+        force = true
+    }
+
+    ctx := generator.Context{
+        ProjectName: projectName,
+        Force: force,
+    }
+
+    fmt.Println("Generating barebones webapp: ", projectName)
+
+    if err := generator.GenerateWebapp(ctx); err != nil {
+        fmt.Println("Error: ", err)
+        return
+    }
+
+    fmt.Println(projectName, " is ready!")
 }
