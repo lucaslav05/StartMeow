8afd8861929a2c6964534acad8447d8124dc8ef7
SJB7788 <sbj7788@gmail.com>
2026-03-15 08:34:26 -0700

    edit main go

--- main.go
+++ main.go
@@ -1,43 +1,49 @@
 package main
 
 import (
-	"StartMeow/internal"
+	internal "StartMeow/internal"
+	models "StartMeow/internal/tui/models"
+	"fmt"
+	"log"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
 )
 
 func main() {
-	// f, err := tea.LogToFile("debug.log", "debug")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer f.Close()
-
-	// questions := []models.Question{
-	// 	models.NewQuestion("Select a language", models.Select, []string{"C", "C#", "Java", "JavaScript"}),
-	// 	models.NewQuestion("Insert project name", models.Prompt, []string{}),
-	// 	models.NewQuestion("Verify project structure", models.Verify, []string{}),
-	// }
-
-	// m := models.NewDefaultModel(questions)
-
-	// p := tea.NewProgram(m)
-	// if _, err := p.Run(); err != nil {
-	// 	fmt.Printf("Error occurred: %v", err)
-	// 	os.Exit(1)
-	// }
-
-	testProj := internal.Project{
-		ProjType:   internal.WebApp,
-		FrontFrame: internal.React,
-		BackFrame:  internal.NextJS,
-		FrontLang:  internal.Javascript,
-		BackLang:   internal.Typescript,
-		Ui:         internal.Download,
-		Database:   internal.MongoDB,
-		ProjName:   "test-project",
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	internal.BuildProject(&testProj)
+	defer f.Close()
+
+	questions := []models.Question{
+		models.NewQuestion("Select a language", internal.Select, []string{"C", "C#", "Java", "JavaScript"}),
+		models.NewQuestion("Insert project name", internal.Field, []string{}),
+		models.NewQuestion("Verify project structure", internal.Info, []string{}),
+	}
+
+	m := models.NewDefaultModel(questions)
+
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error occurred: %v", err)
+		os.Exit(1)
+	}
+
+	// testProj := internal.Project{
+	// 	ProjType:   internal.WebApp,
+	// 	FrontFrame: internal.React,
+	// 	BackFrame:  internal.NextJS,
+	// 	FrontLang:  internal.Javascript,
+	// 	BackLang:   internal.Typescript,
+	// 	Ui:         internal.Download,
+	// 	Database:   internal.MongoDB,
+	// 	ProjName:   "test-project",
+	// }
+
+	// internal.BuildProject(&testProj)
 
 	// 	type Project struct {
 	// 	ProjType   ProjectType
