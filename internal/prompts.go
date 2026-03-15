7a8e71381edbe1b2b3493799599720a84110ff81
evin-gg <evin4369@gmail.com>
2026-03-14 19:23:51 -0700

    refactor (types struct): the types struct are public, prompts.go is unfinished

--- internal/prompts.go
+++ internal/prompts.go
@@ -1,6 +1,42 @@
 package internal
 
-const AppType := Prompt {
-	title: "App Type",
-	promptType: ProjectType,
+// first question
+var AppType = Prompt {
+	Question: "Pick an Application",
+	PromptType: Select,
+	Options: []string{
+		"Web app",
+		"Client/Server",
+		"Mobile",
+		"Terminal"
+	},
 }
+
+// second question
+var FrontendFrameworkType = Prompt {
+	Question: "Frontend Framework Type",
+	PromptType: Select,
+	Options: []string{"Do you want a React frontend framework?"}
+}
+
+// if it's yes queue these prompts
+var BackendFramework = Prompt {
+	Question: "Backend Framework Type",
+	PromptType: "Select",
+	Options: []string{
+		"Choose a backend React framework",
+		"Nextjs",
+		"ReactRouter",
+		"Express"
+	},
+}
+
+// if no, queue the non framework prompts
+var 
+
+
+
+
+
+
+
